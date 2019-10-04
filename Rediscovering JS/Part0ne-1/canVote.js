324ce8aeec1c3e8b49f363a92629a898d08a28b4
Fogha <collinslord50@gmail.com>
2019-10-04 04:19:03 +0100

    Took off rediscovering js folder

--- Rediscovering JS/Part0ne-1/canVote.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const canVote = (age) => {
-  return age >= 18 ? console.log('yes can vote') : console.log('NO cannot vote');
-};
-
-canVote(12);
-//VOte exercise, No errors
--- Rediscovering JS/Part0ne-1/test.js
+++
@@ -1,9 +0,0 @@
-// const fourth = 4;
-// fourth = fourth;
-// console.log(fourth);
-
-'use strict';
-const person = Object.freeze(
-{name: 'John Doe', address: { street: '101 Main St.', City: 'New York' }});
-person.address.street = '102 Main St.';
-console.log(person);
