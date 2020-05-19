3fd496fe3da6d990243b97df9c8a19c67f8b0375
Fogha <collinslord50@gmail.com>
2020-05-19 01:53:46 +0100

    added some functions

--- JS functions/FunctionalJs/lib/es6-functional.js
+++ JS functions/FunctionalJs/lib/es6-functional.js
@@ -1,8 +1,42 @@
-const forEach = (array, fn) => {
+export const forEach = (array, fn) => {
   let i;
   for(i = 0; i < array.length; i++) 
     fn(array[i]);
 }
 
+export const unless = (condition, fn) => {
+  if(condition) 
+    fn()
+}
 
-export default forEach;
+export const times = (times, fn) => {
+  let j;
+  for(j = 0; j < times; j++) 
+    fn(j);
+}
+
+//every and some are inefficient implementations because they should stop at the first item that either 
+//doesnt satisfy or satisfies the condition, hence for very large arrays they are very inefficient.
+
+export const every = (arr, fn) => {
+  let result = true;
+  for(const item of arr) {
+    result = result && fn(item)
+  }
+  return result;
+}
+
+export const some = (arr, fn) => {
+  let result = true;
+  for(const item of arr) {
+    result = result || fn(item);
+  }
+  return result;
+}
+
+export const sortBy = (props) => {
+  return (a, b) => {
+    var result = (a.props < b.props) ? -1 : (a.props > b.props ) ? 1 : 0;
+    return result;
+  }
+}
--- JS functions/FunctionalJs/playground/play.js
+++ JS functions/FunctionalJs/playground/play.js
@@ -1,4 +1,11 @@
-import forEach from '../lib/es6-functional';
+import { forEach, times, unless} from '../lib/es6-functional';
 
-var array = [1,2,3,4];
-forEach(array, (e) => { console.log(e)})
+// var array = [1,2,3,4];
+// forEach(array, (e) => { console.log(e)})
+
+//get all even numbers between 0 and 100
+times(100, (i) => {
+  unless(i % 2 == 0, () => {
+    console.log(i, 'is even');
+  })
+})
