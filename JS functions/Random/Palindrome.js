6014234dd11a65dd08ecf630f8032a22934dc1d4
Fogha <collinslord50@gmail.com>
2020-05-21 00:43:13 +0100

    Worked on the palindrome.js

---
+++ JS functions/Random/Palindrome.js
@@ -0,0 +1,25 @@
+
+
+
+const palindrome = (str) => {
+  let first, midChar, end;
+  let mid = Math.round(str.length/2)
+
+  if(str.length % 2 != 0) {
+    midChar = str.slice(mid-1, mid)
+  }
+
+  first = str.slice(0, mid-1);
+  end = str.slice(mid);
+  newFirst = first.split('').reverse().join('');
+
+  if( newFirst == end) {
+    return true;
+  }
+
+  return false;
+}
+
+
+console.log(palindrome('level'));
+console.log(palindrome('racecar'));
